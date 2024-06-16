c43931593ec061933f56b35d8614e51143517478
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-16 16:49:52 +0000

    Express JS

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 console.dir(app);
+
+app.use(()=>{
+    console.log("We got a new request!");
+})
+
+app.listen(3000, ()=>{
+    console.log("Listening on port 5000")
+});
